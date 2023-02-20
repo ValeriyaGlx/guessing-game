89b0b7c7ba0f2ce252a9ae040d43aa6e93019a9e
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-02-20 15:32:22 +0400

    init: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    this.min = min
+    this.max = max
     }
 
     guess() {
-
+    return this.mid = Math.ceil((this.min + this.max)/2)
     }
 
     lower() {
-
+     this.max = this.mid
     }
 
     greater() {
-
+     this.min = this.mid
     }
 }
 
